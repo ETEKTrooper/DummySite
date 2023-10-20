4022301acf223630c9d182a71c3e4459f16cb4e9
ETEKTrooper <144556198+ETEKTrooper@users.noreply.github.com>
2023-10-19 22:47:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,30 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
-
+const fs = require('fs'); // Add the 'fs' module to read the secret message HTML file
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Database setup
-const db = new sqlite3.Database('userdb.sqlite');
-
-// Create a table for users (if it doesn't exist)
-db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
-});
-
-// Middleware for parsing JSON
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Serve static files (your HTML, CSS, images, etc.)
-app.use(express.static('public'));
-
-// Define routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    // Replace this with a database query to check if the user exists
-    // For demonstration, we're using hardcoded values
-    if (username === 'Estefanus' && password === 'Mikalonte') {
-        res.redirect('/admin.html');
-    } else {
-        // Replace this with your actual error handling logic
-        res.status(401).send('Unauthorized');
-    }
+app.get('/admin', (req, res) => {
+    // Read the content of the secret message HTML file
+    const secretMessageContent = fs.readFileSync(__dirname + '/secret-message.html', 'utf8');
+    
+    const adminPageContent = `
+        <html>
+            <head>
+                <title>Admin Page</title>
+            </head>
+            <body>
+                <h1>Congratulations!</h1>
+                ${secretMessageContent} <!-- Include the secret message here -->
+            </body>
+        </html>
+    `;
+    res.send(adminPageContent);
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
